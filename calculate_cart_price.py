d1e6e93487ca7c4a8b9303203b4417ad3d8c5e23
shashankdass <shashankdass@gmail.com>
2017-08-12 01:18:31 -0700

    Bug Fix!

--- calculate_cart_price.py
+++ calculate_cart_price.py
@@ -4,12 +4,6 @@ import sys
 from base_prices import BasePrices
 from cart import Cart
 
-
-# user_input = True
-sample_carts = ["sample_inputs/cart1.json", "sample_inputs/cart2.json", "sample_inputs/cart3.json",
-                "sample_inputs/cart4.json"]
-default_base_price = "sample_inputs/base_price.json"
-
 arguments = sys.argv
 if len(arguments) == 1:
     print("Please pass complete path of the cart json file followed by base price json file")
@@ -17,9 +11,8 @@ elif len(arguments) < 3:
     print(
         "One of the path is missing. Please pass complete path of the cart json file followed by base price json file")
 else:
-    cart_path = sys.argv[1]  # Uncomment for testing : or sample_carts[random.randint(0, 3)]
-    print("Cart path:{}".format(cart_path))
-    base_price_path = sys.argv[2]  # Uncomment for testing :  or default_base_price
+    cart_path = sys.argv[1]
+    base_price_path = sys.argv[2]
     # Read base price.
     try:
         with open(base_price_path) as data_file:
